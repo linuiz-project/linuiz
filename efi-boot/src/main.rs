e3d33fd3d9005cbb5f94b3b604aee5f055c8ca49
Zavier Divelbiss <semiviral@gmail.com>
2020-12-05 02:01:19 -0600

    integrate uefi-rs package

--- efi-boot/src/main.rs
+++ efi-boot/src/main.rs
@@ -1,24 +1,55 @@
 #![no_std]
 #![no_main]
+#![feature(unsafe_cell_get_mut, negative_impls)]
 
-mod efi;
 
-use core::panic::PanicInfo;
-use efi::{EFIHandle, EFISystemTable, EFIStatus};
+#[macro_use]
+extern crate log;
+
+use uefi::{Handle, proto::{Protocol, loaded_image::LoadedImage}, table::{Boot, SystemTable}};
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[no_mangle]
+pub extern "C" fn efi_main(image: Handle, system_table: SystemTable<Boot>) -> ! {
+    uefi_services::init(&system_table).expect("failed to initialize UEFI services").unwrap();
+
+    info!("Loaded Gsai UEFI bootloader v{}.", VERSION);
+    info!("Configuring kernel environment.");
+
+    let boot_services = system_table.boot_services();
+    boot_services.set_watchdog_timer(0, 0, None).expect("failed to clear watchdog timer");
+    info!("Reset watchdog timer.");
+
+    let image = unsafe { *(boot_services.handle_protocol(image).expect("failed to load image").unwrap().get() as *mut LoadedImage) };
+    info!("Loaded boot image.");
+
+    let device_path = unsafe { *(boot_services.handle_protocol(image.device()).expect("failed to load image device").unwrap().get() as *mut DevicePath) };
+    info!("Loaded boot image device path.");
+    
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn efi_main(_image: EFIHandle, system_table: EFISystemTable) -> EFIStatus {
-    let stdout = system_table.get_console_out();
-    stdout.reset(false).ok();
-    stdout.print_many(&["Loaded Gsai UEFI bootloader v", VERSION, ".\r\n"]).ok();
-    stdout.println("Configuring bootloader environment.");
+#[repr(u8)]
+enum DeviceType {
+    Hardware = 0x01,
+    ACPI = 0x02,
+    Messaging = 0x03,
+    Media = 0x04,
+    BIOSBootSpec = 0x05,
+    End = 0x7F
+}
 
-    loop {}
+#[repr(u8)]
+enum DeviceSubType {
+    EndInstance = 0x01,
+    EndEntire = 0xFF
+}
+
+#[repr(C)]
+#[derive(Protocol)]
+struct DevicePath {
+    device_type: DeviceType,
+    sub_type: DeviceSubType,
+    length: [u8; 2]
 }
