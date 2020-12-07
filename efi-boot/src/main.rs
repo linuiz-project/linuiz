e1e494586b298d54da65e7909939eed98893d113
Zavier Divelbiss <semiviral@gmail.com>
2020-12-06 20:16:17 -0600

    add kernel image file loading

--- efi-boot/src/main.rs
+++ efi-boot/src/main.rs
@@ -8,7 +8,7 @@ extern crate log;
 
 use core::cell::UnsafeCell;
 use uefi_macros::entry;
-use uefi::{CStr16, Handle, Status, prelude::BootServices, proto::{Protocol, media::fs::SimpleFileSystem, loaded_image::{LoadedImage, DevicePath}}, proto::media::file::Directory, proto::media::file::File, proto::media::file::FileInfo, proto::media::file::FileMode, proto::media::file::RegularFile, table::Boot, proto::media::file::FileAttribute, table::SystemTable};
+use uefi::{CStr16, Handle, Status, prelude::BootServices, proto::{Protocol, media::fs::SimpleFileSystem, loaded_image::{LoadedImage, DevicePath}}, proto::media::file::Directory, proto::media::file::File, proto::media::file::FileAttribute, proto::media::file::FileInfo, proto::media::file::FileMode, proto::media::file::RegularFile, table::Boot, proto::media::file::FileProtocolInfo, table::SystemTable};
 
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -20,11 +20,9 @@ fn efi_main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
     info!("Loaded Gsai UEFI bootloader v{}.", VERSION);
     info!("Configuring bootloader environment.");
 
-    info!("Attempting to acquire boot services from UEFI firmware.");
     let boot_services = system_table.boot_services();
     info!("Successfully acquired boot services from UEFI firmware.");
 
-    info!("Attempting to acquire boot image from boot services.");
     let image = get_protocol_unwrap::<LoadedImage>(boot_services, image_handle).expect("failed to acquire boot image");
     info!("Successfully acquired boot image from boot services.");
     let device_path = get_protocol_unwrap::<DevicePath>(boot_services, image.device()).expect("failed to acquire boot image device path");
@@ -36,21 +34,10 @@ fn efi_main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
     let mut root_directory = file_system.open_volume().expect("failed to open boot file system root directory").unwrap();
     info!("Successfully loaded boot file system root directory.");
     
-    let mut buffer = [0u8; 255];
-    let efi_root_directory_info = match read_directory_entry(&mut root_directory, &mut buffer) {
-        Ok(file_info) => file_info,
-        Err(_) => panic!(FAIL_ROOT_READ)
-    };
-    let efi_root_directory: File = efi_root_directory_info.into();
+    let kernel_file = acquire_kernel_file(&mut root_directory);
+    info!("Successfully acquired kernel image file.");
 
-    let kernel_file: RegularFile = match efi_root_directory.open("gsai/kernel.elf", FileMode::Read, FileAttribute::SYSTEM) {
-        Ok(completion) => completion.expect("failed to find kernel file (should be at ELF/gsai/kernel.elf)."),
-        Err(error) => panic!("{:?}", error)
-    }.into_type();
-
-    info!("{}", kernel_file.get_info(&mut buffer).unwrap().unwrap());
-
-
+    
 
     loop {}
 }
@@ -77,6 +64,24 @@ fn acquire_protocol_unwrapped<P: Protocol>(result: uefi::Result<&UnsafeCell<P>, 
         },
         Err(error) => panic!("{:?}", error.status())
     }
+}
+
+fn open_file<F: File>(current: &mut F, name: &str) -> RegularFile {
+    match current.open(name, FileMode::Read, FileAttribute::READ_ONLY) {
+        // this is unsafe due to the possibility of passing an invalid file handle to external code
+        Ok(completion) => unsafe { RegularFile::new(completion.expect("failed to find file")) },
+        Err(error) => panic!("{:?}", error)
+    }
+}
+
+fn print_file_name<F: File>(file: &mut F, buffer: &mut [u8]) {
+    info!("{}", file.get_info::<FileInfo>(buffer).unwrap().unwrap().file_name());
+}
+
+fn acquire_kernel_file<F: File>(root_directory: &mut F) -> RegularFile {
+    let kernel_directory = &mut open_file(root_directory, "EFI");
+    let gsai_directory = &mut open_file(kernel_directory, "gsai");
+    open_file(gsai_directory, "kernel.elf")
 }
 
 fn read_directory_entry<'buf>(directory: &mut Directory, read_buffer: &'buf mut [u8]) -> Result<&'buf mut FileInfo, usize> {
