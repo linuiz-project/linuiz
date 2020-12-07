3f6a877ffe4747b33addf39518b5c4356bf16d35
Zavier Divelbiss <semiviral@gmail.com>
2020-12-06 20:16:38 -0600

    add license, qemu-run.bat to shell script (.sh)

---
+++ qemu-run.sh
@@ -0,0 +1,14 @@
+# exits script on command error
+set -e
+
+# compile and link deps
+make
+
+# run the bootloader image
+qemu-system-x86_64 \
+    -nodefaults \
+    -nographic \
+    -bios \
+    ./ovmf/OVMF-pure-efi.fd \
+    -drive format=raw,file=fat:rw:./image/ \
+    -serial stdio
