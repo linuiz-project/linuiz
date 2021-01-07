0c12eba90ce90082f8e273f2a338457b6861fbaa
Zavier Divelbiss <semiviral@gmail.com>
2021-01-07 13:45:04 -0600

    condense boot.rs code

--- efi_boot/src/boot.rs
+++ efi_boot/src/boot.rs
@@ -14,13 +14,10 @@ mod elf;
 mod file;
 mod kernel_loader;
 mod memory;
-mod protocol;
 
-use crate::{
-    file::open_file,
-    protocol::{get_protocol, locate_protocol},
-};
+use crate::file::open_file;
 use core::{
+    cell::UnsafeCell,
     mem::{size_of, transmute},
     ptr::slice_from_raw_parts_mut,
 };
@@ -32,9 +29,10 @@ use uefi::{
         console::gop::{GraphicsOutput, Mode},
         loaded_image::{DevicePath, LoadedImage},
         media::{
-            file::{Directory, File, RegularFile},
+            file::{Directory, File, FileAttribute, FileMode, RegularFile},
             fs::SimpleFileSystem,
         },
+        Protocol,
     },
     table::{
         boot::{MemoryDescriptor, MemoryType},
@@ -55,6 +53,43 @@ fn configure_log_level() {
 #[cfg(not(debug_assertions))]
 fn configure_log_level() {
     log::set_max_level(log::LevelFilter::Info);
+}
+
+pub fn get_protocol<P: Protocol>(boot_services: &BootServices, handle: Handle) -> Option<&mut P> {
+    acquire_protocol_unwrapped(boot_services.handle_protocol(handle))
+}
+
+pub fn locate_protocol<P: Protocol>(boot_services: &BootServices) -> Option<&mut P> {
+    acquire_protocol_unwrapped(boot_services.locate_protocol::<P>())
+}
+
+fn acquire_protocol_unwrapped<P: Protocol>(result: uefi::Result<&UnsafeCell<P>>) -> Option<&mut P> {
+    if let Ok(completion) = result {
+        if completion.status() == Status::SUCCESS {
+            Some(unsafe { &mut *(completion.unwrap().get()) })
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+pub fn open_file<F: File>(file: &mut F, name: &str) -> RegularFile {
+    debug!("Attempting to load file system object: {}", name);
+    match file.open(name, FileMode::Read, FileAttribute::READ_ONLY) {
+        // this is unsafe due to the possibility of passing an invalid file handle to external code
+        Ok(completion) => unsafe { RegularFile::new(completion.expect("failed to find file")) },
+        Err(error) => panic!("{:?}", error),
+    }
+}
+
+pub fn read_file(file: &mut RegularFile, position: u64, buffer: &mut [u8]) {
+    debug!("Reading file contents into memory (pos {}).", position);
+    file.set_position(position)
+        .expect_success("failed to set position of file");
+    file.read(buffer)
+        .expect_success("failed to read file into memory");
 }
 
 #[entry]
@@ -170,6 +205,117 @@ fn acquire_kernel_file(root_directory: &mut Directory) -> RegularFile {
     kernel_directory.close();
     gsai_directory.close();
     kernel_file
+}
+
+pub fn load_kernel(boot_services: &BootServices, mut kernel_file: RegularFile) -> usize {
+    let kernel_header = acquire_kernel_header(&mut kernel_file);
+    info!("Kernel header read into memory.");
+    debug!("{:?}", kernel_header);
+
+    allocate_segments(boot_services, &mut kernel_file, &kernel_header);
+    info!("Kernel successfully read into memory.");
+
+    kernel_header.entry_address()
+}
+
+fn acquire_kernel_header(kernel_file: &mut RegularFile) -> ELFHeader64 {
+    // allocate a block large enough to hold the header
+    let mut kernel_header_buffer = [0u8; size_of::<ELFHeader64>()];
+
+    // read the file into the buffer
+    kernel_file
+        .read(&mut kernel_header_buffer)
+        .expect_success("failed to read kernel header into memory");
+    let kernel_header =
+        ELFHeader64::parse(&kernel_header_buffer).expect("failed to parse header from buffer");
+
+    kernel_header
+}
+
+fn allocate_segments(
+    boot_services: &BootServices,
+    kernel_file: &mut RegularFile,
+    kernel_header: &ELFHeader64,
+) {
+    let segment_header_buffer = &mut [0u8; size_of::<ProgramHeader>()];
+    let mut segment_header_disk_offset = kernel_header.program_headers_offset();
+
+    for index in 0..kernel_header.program_header_count() {
+        read_file(
+            kernel_file,
+            segment_header_disk_offset as u64,
+            segment_header_buffer,
+        );
+        let segment_header = ProgramHeader::parse(segment_header_buffer)
+            .expect("failed to parse program header from buffer");
+
+        if segment_header.ph_type() == ProgramHeaderType::PT_LOAD {
+            debug!(
+                "Identified loadable segment (index {}, disk offset {}): {:?}",
+                index, segment_header_disk_offset, segment_header
+            );
+
+            // calculate required variables for correctly loading segment into memory
+            let aligned_address = align_down(
+                segment_header.physical_address(),
+                segment_header.alignment(),
+            );
+            // this is the offset within the page that the segment starts
+            let page_offset = wrapping_sub(segment_header.physical_address(), aligned_address);
+            // size of the segment size + offset within the page
+            let aligned_size = page_offset + segment_header.memory_size();
+            let pages_count = aligned_slices(aligned_size, segment_header.alignment());
+
+            debug!(
+                    "Loading segment (index {}):\n Unaligned Address: {}\n Unaligned Size: {}\n Aligned Address: {}\n Aligned Size: {}\n End Address: {}\n Pages: {}",
+                    index, segment_header.physical_address(), segment_header.memory_size(), aligned_address, aligned_size, aligned_address + (pages_count * PAGE_SIZE), pages_count
+                );
+
+            // allocate pages for header
+            let segment_page_buffer = allocate_pages(
+                boot_services,
+                // we take an address relative to kernel insertion
+                // point, but that doesn't really matter to the code
+                // in this context
+                AllocateType::Address(aligned_address),
+                efi_boot::KERNEL_CODE,
+                pages_count,
+            )
+            // we won't ever explicitly deallocate this, so we only
+            // care about the buffer (pointer is used to deallocate, usually)
+            .buffer;
+
+            // the segments might not always be aligned to pages, so take the slice of the buffer
+            // that is equal to the program segment's lowaddr..highaddr
+            let slice_end_index = page_offset + segment_header.disk_size();
+            let segment_slice = &mut segment_page_buffer[page_offset..slice_end_index];
+            // finally, read the program segment into memory
+            read_file(kernel_file, segment_header.offset() as u64, segment_slice);
+
+            // sometimes a segment contains extra space for data, and must be zeroed out before any jumps
+            if segment_header.memory_size() > segment_header.disk_size() {
+                // in this case, we need to zero-out the remaining memory so the segment
+                // doesn't point to garbage data (since we won't be reading anything valid into it)
+                let memory_end_index = page_offset + segment_header.memory_size();
+                debug!(
+                    "Zeroing segment section (index {}): from {} to {}, total {}",
+                    index,
+                    slice_end_index,
+                    memory_end_index,
+                    memory_end_index - slice_end_index
+                );
+
+                for index in slice_end_index..memory_end_index {
+                    segment_page_buffer[index] = 0x0;
+                }
+            }
+
+            debug!("Segment loaded (index {}).", index);
+        }
+
+        // update the segment header offset so we can read next segment
+        segment_header_disk_offset += kernel_header.program_header_size() as usize;
+    }
 }
 
 fn kernel_transfer(
