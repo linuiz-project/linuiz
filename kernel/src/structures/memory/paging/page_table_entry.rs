2932001a2f2ff4aea94fbe685f5e02b20e1dd3a2
Zavier Divelbiss <semiviral@gmail.com>
2021-01-17 18:35:28 -0600

    Create page_table_entry.rs

---
+++ kernel/src/structures/memory/paging/page_table_entry.rs
@@ -0,0 +1,62 @@
+use crate::structures::memory::Frame;
+use bitflags::bitflags;
+
+bitflags! {
+    pub struct PageAttributes : u64 {
+        const PRESENT = 1 << 0;
+        const WRITABLE = 1 << 1;
+        const USER_ACCESSIBLE = 1 << 2;
+        const WRITE_THROUGH = 1 << 3;
+        const DISABLE_CACHE = 1 << 4;
+        const ACCESSED = 1 << 5;
+        const DIRTY = 1 << 6;
+        const HUGE_PAGE = 1 << 7;
+        const GLOBAL = 1 << 8;
+        // 3 bits free for use by OS
+        const NO_EXECUTE = 1 << 63;
+    }
+}
+
+#[repr(transparent)]
+#[derive(Clone, Copy)]
+pub struct PageTableEntry(u64);
+
+impl PageTableEntry {
+    pub const fn unused() -> Self {
+        Self { 0: 0 }
+    }
+
+    pub fn attribs(&self) -> PageAttributes {
+        PageAttributes::from_bits_truncate(self.0)
+    }
+
+    pub fn frame(&self) -> Option<Frame> {
+        if self.is_present() {
+            Some(Frame::from_addr(self.0 & 0x000FFFFF_FFFFF000))
+        } else {
+            None
+        }
+    }
+
+    pub fn set(&mut self, frame: &Frame, attribs: PageAttributes) {
+        self.0 = frame.addr().as_u64() | attribs.bits();
+    }
+
+    pub fn is_present(&self) -> bool {
+        self.attribs().contains(PageAttributes::PRESENT)
+    }
+
+    pub fn set_nonpresent(&mut self) {
+        self.0 &= !PageAttributes::PRESENT.bits();
+    }
+}
+
+impl core::fmt::Debug for PageTableEntry {
+    fn fmt(&self, formatter: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        formatter
+            .debug_tuple("PageDescriptor")
+            .field(&self.frame())
+            .field(&self.attribs())
+            .finish()
+    }
+}
