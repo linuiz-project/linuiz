5d061234b35d5149c4f892844a7998812d4b28a3
Zavier Divelbiss <semiviral@gmail.com>
2021-01-29 03:19:00 -0600

    add timer functionality

---
+++ kernel/src/timer.rs
@@ -0,0 +1,21 @@
+use core::sync::atomic::AtomicUsize;
+
+static TICKS: AtomicUsize = AtomicUsize::new(0);
+static RESOLUTION: AtomicUsize = AtomicUsize::new(8);
+
+fn increment() -> usize {
+    TICKS.fetch_add(1, core::sync::atomic::Ordering::AcqRel)
+}
+
+fn resolution_mask() -> usize {
+    RESOLUTION.load(core::sync::atomic::Ordering::Acquire) - 1
+}
+
+pub fn tick_handler() {
+    let old_ticks = increment();
+    if (old_ticks & resolution_mask()) == 0 {
+        timer_lapse();
+    }
+}
+
+pub fn timer_lapse() {}
