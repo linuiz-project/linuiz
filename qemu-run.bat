b8ad47699676d2674c0762f4e10381c16873f006
Zavier Divelbiss <semiviral@gmail.com>
2021-03-04 01:24:44 -0600

    modify run script to use OVMF drive files (CODE/VARS)

--- qemu-run.bat
+++ qemu-run.bat
@@ -1,6 +1,9 @@
 qemu-system-x86_64^
     -m 256M^
     -serial stdio^
+    -machine q35^
     -cpu qemu64,+x2apic^
-    -bios ./ovmf/OVMF-pure-efi.fd^
-    -drive format=raw,file=fat:rw:./image/
+    -drive format=raw,file=fat:rw:./image/^
+    -drive if=pflash,format=raw,unit=0,file=./ovmf/OVMF_CODE-pure-efi.fd,readonly=on^
+    -drive if=pflash,format=raw,unit=1,file=./ovmf/OVMF_VARS-pure-efi.fd,readonly=on^
+    -net none^
