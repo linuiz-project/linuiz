72d5303177d5f930a6a9b29effdafdb1282c577a
Zavier Divelbiss <semiviral@gmail.com>
2021-09-01 18:56:23 -0500

    move lib.rs macros to macro.rs

---
+++ libkernel/src/macros.rs
@@ -0,0 +1,101 @@
+#[macro_export]
+macro_rules! bitfield_getter_ro {
+    ($field:ident, $getter_name:ident, $bit_index:literal) => {
+        paste::paste! {
+            pub fn [<get_ $getter_name>](&self) -> bool {
+                use bit_field::BitField;
+
+                self.$field.get_bit($bit_index)
+            }
+        }
+    };
+
+    ($field:ident, $field_ty:ty, $var_name:ident, $bit_range:expr) => {
+        paste::paste! {
+            pub fn [<get_ $var_name>](&self) -> $field_ty {
+                use bit_field::BitField;
+
+                self.$field.get_bits($bit_range)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! bitfield_getter {
+    ($field:ident, $var_name:ident, $bit_index:literal) => {
+        paste::paste! {
+            libkernel::bitfield_getter_ro!($field, $var_name, $bit_index);
+
+            pub fn [<set_ $var_name>](&mut self, value: bool) {
+                use bit_field::BitField;
+
+                self.$field.set_bit($bit_index, value);
+            }
+        }
+    };
+
+    ($field:ident, $field_ty:ty, $var_name:ident, $bit_range:expr) => {
+        paste::paste! {
+            libkernel::bitfield_getter_ro!($field, $field_ty, $var_name, $bit_range);
+
+            pub fn [<set_ $var_name>](&mut self, value: $field_ty) {
+                use bit_field::BitField;
+
+                self.$field.set_bits($bit_range, value);
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! atomic_bitfield_getter_ro {
+    ($field:ident, $getter_name:ident, $bit_index:literal) => {
+        paste::paste! {
+            pub fn [<get_ $getter_name>](&self) -> bool {
+                use bit_field::BitField;
+
+                self.$field.load(core::sync::atomic::Ordering::Acquire).get_bit($bit_index)
+            }
+        }
+    };
+
+    ($field:ident, $field_ty:ty, $var_name:ident, $bit_range:expr) => {
+        paste::paste! {
+            pub fn [<get_ $var_name>](&self) -> $field_ty {
+                use bit_field::BitField;
+
+                self.$field.load(core::sync::atomic::Ordering::Acquire).get_bits($bit_range)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! atomic_bitfield_getter {
+    ($field:ident, $var_name:ident, $bit_index:literal) => {
+        libkernel::atomic_bitfield_getter_ro!($field, $var_name, $bit_index);
+
+        paste::paste! {
+            pub fn [<set_ $var_name>](&mut self, set: bool) {
+                use core::sync::atomic::Ordering;
+                use bit_field::BitField;
+
+                self.$field.store(*(self.$field.load(Ordering::Acquire).set_bit($bit_index, set)), Ordering::Release);
+            }
+        }
+    };
+
+    ($field:ident, $field_ty:ty, $var_name:ident, $bit_range:expr) => {
+        paste::paste! {
+            libkernel::atomic_bitfield_getter_ro!($field, $field_ty, $var_name, $bit_range);
+
+            pub fn [<set_ $var_name>](&mut self, value: $field_ty) {
+                use core::sync::atomic::Ordering;
+                use bit_field::BitField;
+
+                self.$field.store(*(self.$field.load(Ordering::Acquire).set_bits($bit_range, value)), Ordering::Release);
+            }
+        }
+    };
+}
