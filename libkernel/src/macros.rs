f89003cc1ad3563ef19b465ecc08521b1e876501
Zavier Divelbiss <semiviral@gmail.com>
2021-09-03 21:45:19 -0500

    move macros to dedicated module

--- libkernel/src/macros.rs
+++ libkernel/src/macros.rs
@@ -99,3 +99,53 @@ macro_rules! atomic_bitfield_getter {
         }
     };
 }
+
+#[macro_export]
+macro_rules! volatile_bitfield_getter_ro {
+    ($field:ident, $getter_name:ident, $bit_index:literal) => {
+        paste::paste! {
+            pub fn [<get_ $getter_name>](&self) -> bool {
+                use bit_field::BitField;
+
+                self.$field.read().get_bit($bit_index)
+            }
+        }
+    };
+
+    ($field:ident, $field_ty:ty, $var_name:ident, $bit_range:expr) => {
+        paste::paste! {
+            pub fn [<get_ $var_name>](&self) -> $field_ty {
+                use bit_field::BitField;
+
+                self.$field.read().get_bits($bit_range)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! volatile_bitfield_getter {
+    ($field:ident, $var_name:ident, $bit_index:literal) => {
+        libkernel::volatile_bitfield_getter_ro!($field, $var_name, $bit_index);
+
+        paste::paste! {
+            pub fn [<set_ $var_name>](&mut self, set: bool) {
+                use bit_field::BitField;
+
+                self.$field.write(*self.$field.read().set_bit($bit_index, set));
+            }
+        }
+    };
+
+    ($field:ident, $field_ty:ty, $var_name:ident, $bit_range:expr) => {
+        paste::paste! {
+            libkernel::volatile_bitfield_getter_ro!($field, $field_ty, $var_name, $bit_range);
+
+            pub fn [<set_ $var_name>](&mut self, value: $field_ty) {
+                use bit_field::BitField;
+
+                self.$field.write(*self.$field.read().set_bits($bit_range, value));
+            }
+        }
+    };
+}
