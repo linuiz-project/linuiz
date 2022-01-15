0350c20c5a9e24c80bcf6e796e686aaaf01e6617
Zavier Divelbiss <semiviral@gmail.com>
2022-01-15 17:04:27 -0600

    parallelize AP kernel init

--- kernel/src/local_state/int_ctrl/mod.rs
+++ kernel/src/local_state/int_ctrl/mod.rs
@@ -1,4 +1,4 @@
-mod handlers;
+pub(crate) mod handlers;
 
 use core::sync::atomic::AtomicU64;
 use libstd::structures::apic::APIC;
@@ -19,8 +19,6 @@ pub enum InterruptVector {
     Spurious = u8::MAX,
 }
 
-static INT_CTRL_CREATE_LOCK: spin::Mutex<u8> = spin::Mutex::new(0);
-
 pub struct InterruptController {
     apic: APIC,
     counters: [AtomicU64; 256],
@@ -30,10 +28,7 @@ impl InterruptController {
     const ATOMIC_ZERO: AtomicU64 = AtomicU64::new(0);
 
     pub fn create() -> Self {
-        use libstd::structures::{apic::*, idt};
-
-        idt::set_handler_fn(InterruptVector::Spurious as u8, handlers::spurious_handler);
-        idt::set_handler_fn(InterruptVector::Error as u8, handlers::error_handler);
+        use libstd::structures::apic::*;
 
         // Ensure interrupts are enabled.
         libstd::instructions::interrupts::enable();
@@ -41,41 +36,29 @@ impl InterruptController {
         debug!("Configuring APIC & APIT.");
         let apic = APIC::from_msr().expect("APIC has already been configured on this core");
         unsafe {
-            apic.hw_enable();
             apic.reset();
+            apic.hw_enable();
         }
         apic.write_register(Register::TimerDivisor, TimerDivisor::Div1 as u32);
         apic.timer().set_vector(InterruptVector::LocalTimer as u8);
         apic.timer().set_mode(TimerMode::OneShot);
 
-        {
-            // Lock here, to ensure swaps of the handler at the APIT IDT offset aren't intermingled.
-            let lock = INT_CTRL_CREATE_LOCK.lock();
-
-            extern "x86-interrupt" fn apit_counter_deplete_error(_: idt::InterruptStackFrame) {
-                panic!("APIT has fired an interrupt during initialization (NOTE: APIT should not have enough time to fully deplete its counter register).");
-            }
-
-            idt::set_handler_fn(
-                InterruptVector::LocalTimer as u8,
-                apit_counter_deplete_error,
-            );
-
+        let ticks_per_10ms = {
             debug!("Determining APIT frequency.");
+
+            // Wait on the global timer, to ensure we're starting the count on the rising edge of each millisecond.
+            crate::clock::global::busy_wait_msec(1);
             // 'Enable' the APIT to begin counting down in `Register::TimerCurrentCount`
             apic.write_register(Register::TimerInitialCount, u32::MAX);
             // Wait for 10ms to get good average tickrate.
-            crate::clock::global::sleep_msec(10);
+            crate::clock::global::busy_wait_msec(10);
 
-            idt::set_handler_fn(InterruptVector::LocalTimer as u8, handlers::apit_handler);
-
-            // Drop lock manually, to (hopefully) ensure it isn't optimized away (?).
-            drop(lock);
-        }
+            apic.read_register(Register::TimerCurrentCount)
+        };
 
         apic.write_register(
             Register::TimerInitialCount,
-            (u32::MAX - apic.read_register(Register::TimerCurrentCount)) / 10,
+            (u32::MAX - ticks_per_10ms) / 10,
         );
         debug!(
             "APIT frequency: {}KHz",
--- kernel/src/local_state/mod.rs
+++ kernel/src/local_state/mod.rs
@@ -7,7 +7,6 @@ use crate::clock::AtomicClock;
 use core::sync::atomic::AtomicUsize;
 use libstd::registers::MSR;
 
-const MAGIC: u64 = 0xF;
 pub static INIT_COUNT: AtomicUsize = AtomicUsize::new(0);
 
 pub fn init() {
@@ -17,41 +16,40 @@ pub fn init() {
         "IA32_FS MSR has already been configured."
     );
 
+    INIT_COUNT.fetch_add(1, core::sync::atomic::Ordering::Relaxed);
+
     let apic_id = {
         libstd::instructions::cpuid::exec(0x1, 0x0)
             .unwrap()
             .ebx()
             .get_bits(24..) as u64
     };
-    debug!("Configuring LPU state: {}.", apic_id);
+    debug!("Configuring local state: {}.", apic_id);
 
     unsafe {
-        let lpu_ptr = alloc::alloc::alloc_zeroed(
-            core::alloc::Layout::from_size_align(core::mem::size_of::<LocalState>(), 0x1000)
-                .unwrap(),
-        ) as *mut LocalState;
+        let lpu_ptr = libstd::memory::malloc::try_get()
+            .unwrap()
+            .alloc(
+                core::mem::size_of::<LocalState>(),
+                // Align to 0x1000 to accomodate some state bits.
+                core::num::NonZeroUsize::new(0x1000),
+            )
+            .unwrap()
+            .cast::<LocalState>()
+            .unwrap()
+            .into_parts()
+            .0;
 
         // Write the LPU structure into memory.
-        lpu_ptr.write_volatile(LocalState {
+        lpu_ptr.write(LocalState {
             clock: AtomicClock::new(),
             int_ctrl: InterruptController::create(),
         });
 
         // Convert ptr to 64 bit representation, and write metadata into low bits.
-        MSR::IA32_FS_BASE.write(
-            *(lpu_ptr as u64)
-                .set_bits(0..4, MAGIC)
-                .set_bits(4..12, apic_id),
-        );
-        trace!("IA32_FS updated: 0x{:X}.", MSR::IA32_FS_BASE.read());
-        INIT_COUNT.fetch_add(1, core::sync::atomic::Ordering::Relaxed);
-
+        MSR::IA32_FS_BASE.write(lpu_ptr as u64 | apic_id);
         int_ctrl().unwrap().sw_enable();
     }
-}
-
-pub fn is_bsp() -> bool {
-    MSR::IA32_APIC_BASE.read().get_bit(8)
 }
 
 struct LocalState {
@@ -59,9 +57,9 @@ struct LocalState {
     int_ctrl: InterruptController,
 }
 
-fn check_magic_and_read_fs_base() -> Option<u64> {
+fn get_fs_base() -> Option<u64> {
     let fs_base = MSR::IA32_FS_BASE.read();
-    if fs_base.get_bits(0..4) == MAGIC {
+    if fs_base > 0 {
         Some(fs_base)
     } else {
         None
@@ -69,12 +67,15 @@ fn check_magic_and_read_fs_base() -> Option<u64> {
 }
 
 fn try_get_lpu() -> Option<&'static LocalState> {
-    check_magic_and_read_fs_base()
-        .and_then(|fs_base| unsafe { ((fs_base & !0xFFF) as *const LocalState).as_ref() })
+    get_fs_base().and_then(|fs_base| unsafe { ((fs_base & !0xFFF) as *const LocalState).as_ref() })
+}
+
+pub fn is_bsp() -> bool {
+    MSR::IA32_APIC_BASE.read().get_bit(8)
 }
 
 pub fn id() -> Option<u8> {
-    check_magic_and_read_fs_base().map(|fs_base| fs_base.get_bits(4..12) as u8)
+    get_fs_base().map(|fs_base| fs_base.get_bits(4..12) as u8)
 }
 
 pub fn clock() -> Option<&'static AtomicClock> {
