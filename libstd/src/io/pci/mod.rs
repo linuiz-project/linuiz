8a31923b21d7dbcd78012d02d648bcdc6979a89b
avirule <semiviral@gmail.com>
2022-01-18 07:08:30 -0600

    add `pci::BRIDGES` lazy_static

--- libstd/src/io/pci/mod.rs
+++ libstd/src/io/pci/mod.rs
@@ -5,3 +5,28 @@ mod host;
 pub use bus::*;
 pub use device::*;
 pub use host::*;
+
+use alloc::vec::Vec;
+use spin::Mutex;
+
+pub struct Bridges(Mutex<Vec<PCIeHostBridge>>);
+unsafe impl Send for Bridges {}
+unsafe impl Sync for Bridges {}
+impl core::ops::Deref for Bridges {
+    type Target = Mutex<Vec<PCIeHostBridge>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+lazy_static::lazy_static! {
+    pub static ref BRIDGES: Bridges = Bridges(spin::Mutex::new(
+        crate::acpi::rdsp::xsdt::XSDT
+            .find_sub_table::<crate::acpi::rdsp::xsdt::mcfg::MCFG>()
+            .expect("XSDT does not contain an MCFG table")
+            .iter()
+            .filter_map(|entry| configure_host_bridge(entry).ok())
+            .collect(),
+    ));
+}
