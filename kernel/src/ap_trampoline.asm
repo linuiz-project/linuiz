d3c082147b0ea4838667a1371dcda9be3ce6cc31
avirule <semiviral@gmail.com>
2022-02-06 20:37:09 -0600

    add error checking & fallback to ap trampoline

--- kernel/src/ap_trampoline.asm
+++ kernel/src/ap_trampoline.asm
@@ -7,17 +7,29 @@ realmode:
     cli
     cld
 
-    ; Enable PAE
     mov eax, cr4
-    or eax, 1 << 5
+    or eax, 1 << 5 ; Enable PAE
+    or eax, 1 << 7 ; Enable PGE 
     mov cr4, eax
 
     ; Set PML4 address
     mov eax, [__kernel_pml4]
     mov cr3, eax
 
+    ; Check for NXE support.
+    mov eax, 0x80000001
+    cpuid
+    and edx, 1 << 20    ; Check if EFER.NXE bit is available
+    jz nxe_enable_fail  ; If not, skip setting it in IA32_EFER.
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 11     ; NXE bit in IA32_EFER
+    wrmsr               
+
+    nxe_enable_fail:
+
     ; Enable long mode
-    mov ecx, 0xC0000080     ; Set correct MSR
+    mov ecx, 0xC0000080     ; IA32_EFER MSR
     rdmsr
     or eax, 1 << 8          ; Set LME bit
     wrmsr
@@ -27,49 +39,106 @@ realmode:
     or eax, 1 << 31 | 1 << 0
     mov cr0, eax
 
-    ; Serialize pipeline
+    ; Serialize pipeline after mode switch
     cpuid
 
     ; Set GDT & long-jump to long mode
     lgdt [__gdt.pointer]
-    jmp 0x08:longmode
+    jmp __gdt.code:longmode
+
+
+extern __bsp_init_complete
 
 bits 64
 longmode:
-    cli
-
     ; Update segment registers  
-    mov ax, 0x10
+    mov ax, __gdt.data
     mov ss, ax
     ; Clear unused segments
     xor ax, ax
     mov es, ax
+    mov ds, ax
     mov fs, ax
     mov gs, ax
-    mov ss, ax
 
-    ; Get APIC ID (this acts as an __ap_stack_pointers index)
-    mov eax, 0x1
-    cpuid                           ; The APIC ID is in bits 24..32 (exclusive range)
-    shr ebx, 24
-    and ebx, 0xFF                   ; `ebx` or `bl` now contains the APIC ID, so we chop off the sign-extended bits
-    mov eax, 0x8                    ; Native integer width
-    mul ebx                         ; `eax` now contains byte offset of APIC ID (relative to `__ap_stack_pointers`)
-    add eax, __ap_stack_pointers    ; `eax` now contains the absolute offset of the AP stack pointer
+    x2_apic_id:
+        ; Test for support (eax > 0)
+        mov eax, 0x1F
+        cpuid
+        or eax, ebx
+        or eax, ecx
+        or eax, edx
+        test eax, eax
+        ; If CPUID not supported, try next leaf.
+        jz x2_apic_id_backup
+        ; Otherwise, APIC ID is stored in `edx`.
+        jmp set_rsp
+    x2_apic_id_backup:
+        mov eax, 0xB
+        cpuid
+        or eax, ebx
+        or eax, ecx
+        or eax, edx
+        test eax, eax
+        ; If CPUID not supported, try next leaf.
+        jz apic_id_legacy
+        ; Otherwise, APIC ID is stored in `edx`.
+        jmp set_rsp
+    apic_id_legacy:
+        ; No advanced APIC IDs are available, so rely on legacy 8-bit ID.
+        mov eax, 0x1
+        cpuid
+        shr ebx, 24     ; APIC ID is in bits 24..32
+        and ebx, 0xFF   ; `ebx` or `bl` now contains the APIC ID, so truncate any sign-extended bits
+        mov ebx, edx
+        jmp set_rsp
 
-    mov rsp, [eax]
+
+
+    set_rsp:
+        lea rcx, [__ap_stack_pointers + (rdx * 8)]
+        mov rsp, [rcx]
+
 
     ; Jump to high-level code
     call _startup
 
-
 section .ap_data
 
-global __kernel_pml4, __gdt
+global __kernel_pml4
 __kernel_pml4 resd 1
+
+; Access bits
+PRESENT        equ 1 << 7
+NOT_SYS        equ 1 << 4
+EXEC           equ 1 << 3
+DC             equ 1 << 2
+RW             equ 1 << 1
+USER           equ 3 << 5
+ACCESSED       equ 1 << 0
+
+; Flags bits
+GRAN_4K       equ 1 << 7
+; This flag should not be present with LONG_MODE flag.
+; They are mutually excuslive.
+SZ_32         equ 1 << 6
+LONG_MODE     equ 1 << 5
+
 __gdt:
-    resq 7
-    
+    .null: equ $ - __gdt
+        dq 0
+    .code: equ $ - __gdt
+        dd 0xFFFF                           ; Limit & Base (low)
+        db 0                                ; Base (mid)
+        db PRESENT | NOT_SYS | EXEC | RW    ; Access
+        db GRAN_4K | LONG_MODE | 0xF        ; Flags
+        db 0                                ; Base (high)
+    .data: equ $ - __gdt
+        dd 0xFFFF                           ; Limit & Base (low)
+        db 0                                ; Base (mid)
+        db PRESENT | NOT_SYS | RW           ; Access
+        db GRAN_4K | SZ_32 | 0xF            ; Flags
+        db 0                                ; Base (high)    
     .pointer:
         dw $ - __gdt - 1
         dq __gdt
