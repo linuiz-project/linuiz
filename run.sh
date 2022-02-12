37cc13a31924356364df75e5b19960500e08914f
avirule <semiviral@gmail.com>
2022-02-12 01:44:30 -0600

    lazy commit

--- run.sh
+++ run.sh
@@ -9,9 +9,5 @@ qemu-system-x86_64 \
     -serial mon:stdio \
     -display none \
     -net none \
-    -bios ./ovmf.fd \
-    -drive format=raw,file=fat:rw:./.hdd/image/ \
-    -drive format=raw,file=./.hdd/nvme.img,id=nvm,if=none \
-    -device nvme,drive=nvm,serial=deadbeef \
     -D .debug/qemu_debug.log \
     -d int,guest_errors
