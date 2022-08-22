d17a62b85ee9abd43d4894b5625f483af7aeed54
Zavier Divelbiss <semiviral@gmail.com>
2022-08-22 12:22:07 -0500

    cleanup `Once` usage,  avoid get with `call_once`

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -1,15 +1,16 @@
 mod slob;
 
+use core::cell::SyncUnsafeCell;
+
 pub use slob::*;
 
 use libkernel::memory::{FrameManager, PageManager};
 use libkernel::{Address, Virtual};
 use spin::Once;
 
-static LIMINE_MMAP: limine::LimineMmapRequest = limine::LimineMmapRequest::new(crate::LIMINE_REV);
-static LIMINE_HHDM: limine::LimineHhdmRequest = limine::LimineHhdmRequest::new(crate::LIMINE_REV);
-
 fn get_limine_mmap() -> &'static [limine::LimineMemmapEntry] {
+    static LIMINE_MMAP: limine::LimineMmapRequest = limine::LimineMmapRequest::new(crate::LIMINE_REV);
+
     LIMINE_MMAP
         .get_response()
         .get()
@@ -18,34 +19,44 @@ fn get_limine_mmap() -> &'static [limine::LimineMemmapEntry] {
         .expect("bootloader provided no memory map entries")
 }
 
-static HHDM_ADDR: Once<Address<Virtual>> = Once::new();
-// Gets the kernel's higher half direct mapping page.
-pub fn get_kernel_hhdm_address() -> Address<Virtual> {
-    *HHDM_ADDR.call_once(|| {
+pub static HHDM_ADDRESS: Once<Address<Virtual>> = Once::new();
+/// SAFETY: This function assumes it will be called before the kernel takes ownership of the page tables.
+pub unsafe fn init_kernel_hhdm_address() {
+    HHDM_ADDRESS.call_once(|| {
+        static LIMINE_HHDM: limine::LimineHhdmRequest = limine::LimineHhdmRequest::new(crate::LIMINE_REV);
+
         Address::<Virtual>::new(
             LIMINE_HHDM.get_response().get().expect("bootloader provided no higher-half direct mapping").offset
                 as usize,
         )
         .expect("bootloader provided an invalid higher-half direct mapping address")
-    })
+    });
+}
+pub fn get_kernel_hhdm_address() -> Address<Virtual> {
+    unsafe { *HHDM_ADDRESS.get().unwrap() }
 }
 
 static KERNEL_FRAME_MANAGER: Once<FrameManager> = Once::new();
-/// Gets the kernel frame manager.
+/// SAFETY: This function assumes it will be called before the kernel takes ownership of the page tables.
+pub unsafe fn init_kernel_frame_manager() {
+    KERNEL_FRAME_MANAGER.call_once(|| FrameManager::from_mmap(get_limine_mmap(), get_kernel_hhdm_address()));
+}
 pub fn get_kernel_frame_manager() -> &'static FrameManager<'static> {
-    KERNEL_FRAME_MANAGER.call_once(|| FrameManager::from_mmap(get_limine_mmap(), get_kernel_hhdm_address()))
+    KERNEL_FRAME_MANAGER.get().unwrap()
 }
 
 static KERNEL_PAGE_MANAGER: Once<PageManager> = Once::new();
-
-pub fn get_kernel_page_manager() -> &'static PageManager {
+pub fn init_kernel_page_manager() {
     KERNEL_PAGE_MANAGER.call_once(|| unsafe {
         PageManager::new(
             get_kernel_frame_manager(),
             &libkernel::memory::Page::from_index(get_kernel_hhdm_address().page_index()),
             None,
         )
-    })
+    });
+}
+pub fn get_kernel_page_manager() -> &'static PageManager {
+    KERNEL_PAGE_MANAGER.get().unwrap()
 }
 
 pub fn reclaim_bootloader_memory() {
