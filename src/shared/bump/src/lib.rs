bff20b0117c6645b85d87e5cf86ce969e6986a73
Zavier Divelbiss <semiviral@gmail.com>
2022-11-25 02:14:55 -0600

    build out freelist

--- src/shared/bump/src/lib.rs
+++ src/shared/bump/src/lib.rs
@@ -1,50 +1,61 @@
 #![no_std]
-#![feature(allocator_api)]
+#![feature(allocator_api, int_roundings, slice_ptr_get)]
 
 use core::{
     alloc::{AllocError, Allocator, Layout},
     ptr::NonNull,
-    sync::atomic::{AtomicBool, AtomicUsize, Ordering},
 };
 
+struct Bump {
+    ptr: NonNull<[u8]>,
+    offset: usize,
+}
+
 pub struct BumpAllocator<A: Allocator> {
-    ptr: spin::Mutex<(usize, NonNull<[u8]>)>,
+    bump: spin::Mutex<Bump>,
     allocator: A,
 }
 
 impl<A: Allocator> BumpAllocator<A> {
-    fn with_ptr_lock<T>(&self, func: impl FnOnce(&mut (usize, NonNull<[u8]>)) -> T) {
-        let ptr_lock = self.ptr.lock();
+    pub fn new_in(initial_size: usize, allocator: A) -> Result<Self, AllocError> {
+        Ok(Self {
+            bump: spin::Mutex::new(Bump {
+                ptr: allocator.allocate(Layout::array::<u8>(initial_size).map_err(|_| AllocError)?)?,
+                offset: 0,
+            }),
+            allocator,
+        })
+    }
+
+    fn with_ptr_lock<T>(&self, func: impl FnOnce(&mut Bump) -> T) -> T {
+        let mut ptr_lock = self.bump.lock();
         func(&mut *ptr_lock)
     }
 }
 
 unsafe impl<A: Allocator> Allocator for BumpAllocator<A> {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        self.with_ptr_lock(|(offset, ptr)| {
-            use core::mem::align_of;
-
+        self.with_ptr_lock(|bump| {
             let layout_size = layout.pad_to_align().size();
-            let aligned_offset = offset.next_multiple_of(layout.align());
+            let aligned_offset = bump.offset.next_multiple_of(layout.align());
             let end_offset = aligned_offset + layout_size;
 
-            if end_offset > ptr.len() {
-                *ptr = unsafe {
+            if end_offset > bump.ptr.len() {
+                bump.ptr = unsafe {
                     self.allocator.grow(
-                        *ptr,
-                        Layout::array::<u8>(ptr.len()).map_err(|_| AllocError)?,
+                        bump.ptr.as_non_null_ptr(),
+                        Layout::array::<u8>(bump.ptr.len()).map_err(|_| AllocError)?,
                         Layout::array::<u8>(end_offset.next_power_of_two()).map_err(|_| AllocError)?,
                     )?
                 };
             }
 
-            if end_offset > ptr.len() {
-                Err(AllocError)
-            } else {
-                *offset = end_offset;
-
-                Ok(unsafe { ptr.get_unchecked_mut(aligned_offset..end_offset) })
-            }
+            bump.offset = end_offset;
+            Ok(unsafe { bump.ptr.get_unchecked_mut(aligned_offset..end_offset) })
         })
+    }
+
+    unsafe fn deallocate(&self, _ptr: NonNull<u8>, _layout: Layout) {
+        todo!()
     }
 }
--- src/shared/freelist/src/lib.rs
+++ src/shared/freelist/src/lib.rs
@@ -3,21 +3,83 @@
 
 extern crate alloc;
 
-use core::{alloc::{Allocator, Layout, AllocError}, ptr::NonNull};
+use alloc::collections::{BTreeMap, BTreeSet};
+use core::{
+    alloc::{AllocError, Allocator, Layout},
+    ptr::NonNull,
+};
 
-use alloc::collections::BTreeMap;
-
-struct Region {
-    is_free: bool,
-    size: usize
+#[derive(Debug, Clone, Copy)]
+struct Span {
+    memory: NonNull<[u8]>,
+    refs: usize,
 }
 
-pub struct FreeListAllocator<A: Allocator> {
-    list: BTreeMap<Region, A>,
-}
-
-unsafe impl<A: Allocator> Allocator for FreeListAllocator<A> {
-    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        
+impl Eq for Span {}
+impl PartialEq for Span {
+    fn eq(&self, other: &Self) -> bool {
+        self.memory.eq(&other.memory)
     }
-} 
+}
+
+impl Ord for Span {
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        self.memory.cmp(&other.memory)
+    }
+}
+
+impl PartialOrd for Span {
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
+        self.memory.partial_cmp(&other.memory)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Segment {
+    start: usize,
+    len: usize,
+    span: Span,
+}
+
+impl Eq for Segment {}
+impl PartialEq for Segment {
+    fn eq(&self, other: &Self) -> bool {
+        self.start.eq(&other.start) && self.len.eq(&other.len)
+    }
+}
+
+impl Ord for Segment {
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        match self.len.cmp(&other.len) {
+            core::cmp::Ordering::Equal => self.start.cmp(&other.start),
+            ordering => ordering
+        }
+    }
+}
+
+impl PartialOrd for Segment {
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+struct FreeList<A: Allocator + Clone> {
+    spans: BTreeSet<Span, A>,
+    segments: BTreeSet<Segment, A>,
+    allocation_tags: BTreeMap<NonNull<u8>, Span, A>,
+}
+
+pub struct FreeListAllocator<A: Allocator + Clone>{
+    freelist: spin::Mutex<FreeList<A>>,
+    allocator: A
+}
+
+unsafe impl<A: Allocator + Clone> Allocator for FreeListAllocator<A> {
+    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        let freelist = self.freelist.lock();
+
+        loop {
+            let segment = freelist.segments.iter().find(|ptr| ptr.len() > )
+        }
+    }
+}
