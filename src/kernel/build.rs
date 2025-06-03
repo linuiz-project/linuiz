ab916bc92220d391cbe633ca8b8135824dd19c7a
zdivelbiss <8019570+zdivelbiss@users.noreply.github.com>
2025-06-03 08:23:31 -0500

    add cargo configs

--- src/kernel/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!(
-        "cargo:rustc-link-arg=--script={}/lds/{}.lds",
-        std::env::var("CARGO_MANIFEST_DIR").unwrap(),
-        std::env::var("TARGET").unwrap()
-    );
-}
