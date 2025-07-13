c59fa882ee8989d342ee9b7baf8601f355790204
zdivelbiss <8019570+zdivelbiss@users.noreply.github.com>
2025-07-13 18:40:20 -0500

    require mega page alignments

--- src/kernel/build.rs
+++ src/kernel/build.rs
@@ -1,8 +1,5 @@
 fn main() {
-    if let Ok(kernel_segment_align) = std::env::var("KERNEL_SEGMENT_ALIGN") {
-        println!("cargo:rustc-link-arg=-zmax-page-size={kernel_segment_align}");
-    }
-
+    println!("cargo::rustc-link-arg=-zmax-page-size=0x200000");
     println!(
         "cargo:rustc-link-arg=--script={}/lds/{}.lds",
         std::env::var("CARGO_MANIFEST_DIR").expect("`CARGO_MANIFEST_DIR` must be provided"),
