ed8e27fbd8d7377892ee574a5e304fc85f34b022
zdivelbiss <8019570+zdivelbiss@users.noreply.github.com>
2025-08-07 12:27:40 -0500

    improve build script, x86 irq to assembly

--- src/kernel/build.rs
+++ src/kernel/build.rs
@@ -1,8 +1,12 @@
 fn main() {
     println!("cargo::rustc-link-arg=-zmax-page-size=0x200000");
-    println!(
-        "cargo:rustc-link-arg=--script={}/lds/{}.lds",
-        std::env::var("CARGO_MANIFEST_DIR").expect("`CARGO_MANIFEST_DIR` must be provided"),
-        std::env::var("TARGET").expect("`TARGET` must be provided")
-    );
+
+    let manifest_dir =
+        std::env::var("CARGO_MANIFEST_DIR").expect("`CARGO_MANIFEST_DIR` must be provided");
+    let target_triple = std::env::var("TARGET").expect("`TARGET` must be provided");
+
+    println!("cargo:rustc-link-arg=-T{manifest_dir}/build/{target_triple}.lds");
+
+    println!("cargo::rerun-if-changed={manifest_dir}/build/{target_triple}.a");
+    println!("cargo::rustc-link-arg={manifest_dir}/build/{target_triple}.a");
 }
